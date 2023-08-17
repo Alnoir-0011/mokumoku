b669c33a468067c64778173441cee94b8ce90539
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2023-08-17 17:00:39 +0900

    Add: user_profies

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,5 +1,10 @@
 class UserProfilesController < ApplicationController
+  before_action :require_login
+  layout 'layouts/user_profile/application'
   def show
     @user = User.find(params[:id])
+    @bookmark_events = @user.bookmark_events
+    @future_events = @user.attend_events.future
+    @past_events = @user.attend_events.past
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,5 @@ Rails.application.routes.draw do
       resources :events, only: %i[index]
     end
   end
+  resources :user_profiles, only: %i[show]
 end
